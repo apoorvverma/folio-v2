284ba7b5db706b3f5c64a7c0feaa9862991edb7b
Apoorv Verma <apoorv.verma999@gmail.com>
2025-01-21 18:23:09 -0500

    Removed animations from skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,9 +4,9 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
-const fadeInAnimationVariants = {
+/* const fadeInAnimationVariants = {
   initial: {
     opacity: 0,
     y: 100,
@@ -18,7 +18,7 @@ const fadeInAnimationVariants = {
       delay: 0.05 * index,
     },
   }),
-};
+}; */
 
 export default function Skills() {
   const { ref } = useSectionInView("Skills");
@@ -32,19 +32,19 @@ export default function Skills() {
       <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
-          <motion.li
+          <li
             className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
             key={index}
-            variants={fadeInAnimationVariants}
+            /* variants={fadeInAnimationVariants}
             initial="initial"
             whileInView="animate"
             viewport={{
               once: true,
             }}
-            custom={index}
+            custom={index} */
           >
             {skill}
-          </motion.li>
+          </li>
         ))}
       </ul>
     </section>
